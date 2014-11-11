6336664f7e879c1ed1494d68da89d65a7e5e93a6
tiantianquan <fenqiufen@gmail.com>
2014-11-12 00:53:03 +0800

    不用任何库的scroll写法

--- js/script.js
+++ js/script.js
@@ -20,15 +20,18 @@ for (var i = 0; i < lis.length; i++) {
     var startTop = body.scrollTop
     var endTop = document.querySelector(this.hash).offsetTop
     var gapPx = getGapPx(startTop, endTop);
-    a = setInterval(function() {
-      body.scrollTop += gapPx;
-      if(gapPx>0&&body.scrollTop>=endTop)
-        window.clearInterval(a);
-      if(gapPx<0&&body.scrollTop<=endTop)
-        window.clearInterval(a);
-    }, 15)
 
-    // requestAnimationFrame(animateFn)
+    function animateFn() {
+      body.scrollTop += gapPx;
+      if (gapPx > 0 && body.scrollTop >= endTop)
+        return;
+      else if (gapPx < 0 && body.scrollTop <= endTop)
+        return;
+      else
+        requestAnimationFrame(animateFn)
+    }
+
+    requestAnimationFrame(animateFn)
 
   })
 }
@@ -38,7 +41,9 @@ for (var i = 0; i < lis.length; i++) {
 
 
 function getGapPx(startTop, endTop) {
-  var gapTime = 15
-  var wholeTime = 1000
-  return (endTop - startTop) * (gapTime / wholeTime)
+  // var gapTime = 5
+  // var wholeTime = 1000
+  // return (endTop - startTop) * (gapTime / wholeTime)
+  var gapPx = 20
+  return endTop - startTop >= 0 ? gapPx : -gapPx;
 }
